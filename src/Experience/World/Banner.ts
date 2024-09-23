f5dfea32aabf604d043220d0553f2e9dfc438979
Chae Lin Shin <chaelinshin96@gmail.com>
2024-09-24 07:07:51 +0900

    Update Banner - move initBanner&updateBanner code to the Banner class

--- src/Experience/World/Banner.ts
+++ src/Experience/World/Banner.ts
@@ -1,90 +1,143 @@
 import * as THREE from "three";
-
 import Experience from "../Experience";
-import {Font, FontLoader} from 'three/examples/jsm/loaders/FontLoader.js';
-import {TextGeometry} from "three/examples/jsm/geometries/TextGeometry.js";
-
-class customBannerGroup extends THREE.Group {
-    operator?: string;
-    value?: number;
-}
+import { Font, FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
+import Character from './Character';
 
 export default class Banner {
     experience: Experience;
-    poleGeometry?: THREE.CylinderGeometry;
-    planeGeometry?: THREE.PlaneGeometry;
+    group: THREE.Group;
+    static banners: THREE.Group[] = [];
+    static lastBannerTime: number = 0;
+    static bannerInterval: number = 2500; // 2.5 seconds
+
     poleMaterial?: THREE.MeshBasicMaterial;
     planeMaterial?: THREE.MeshBasicMaterial;
-    group: customBannerGroup;
+
+    static poleGeometry?: THREE.CylinderGeometry;
+    static planeGeometry?: THREE.PlaneGeometry;
+    static font?: Font;
 
     constructor() {
         this.experience = Experience.getInstance();
         this.group = new THREE.Group();
-        this.group.operator = this.getRandomOperator();
-        this.group.value = this.getRandomValue();
-
         this.setGeometry();
         this.setMaterial();
         this.setMesh();
     }
 
     setGeometry() {
-        this.poleGeometry = new THREE.CylinderGeometry(0.3, 0.3, 5, 32);
-        this.planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+        if (!Banner.poleGeometry) {
+            Banner.poleGeometry = new THREE.CylinderGeometry(0.3, 0.3, 5, 32);
+        }
+        if (!Banner.planeGeometry) {
+            Banner.planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+        }
     }
 
     setMaterial() {
-        this.poleMaterial = new THREE.MeshBasicMaterial({
-            color: this.getRandomColor(),
-            wireframe: false,
-        });
-
-        this.planeMaterial = new THREE.MeshBasicMaterial({color: 'white', transparent: true, opacity: 0.2});
+        if (!this.poleMaterial) {
+            this.poleMaterial = new THREE.MeshBasicMaterial({ color: this.getRandomColor() });
+        }
+        if (!this.planeMaterial) {
+            this.planeMaterial = new THREE.MeshBasicMaterial({ color: 'white', transparent: true, opacity: 0.2 });
+        }
     }
 
-    setMesh() {
-        const pole1 = new THREE.Mesh(this.poleGeometry, this.poleMaterial);
+    async setMesh() {
+        const pole1 = new THREE.Mesh(Banner.poleGeometry, this.poleMaterial);
         pole1.position.set(0, 0, 0);
-
-        const pole2 = new THREE.Mesh(this.poleGeometry, this.poleMaterial);
+        const pole2 = new THREE.Mesh(Banner.poleGeometry, this.poleMaterial);
         pole2.position.set(5, 0, 0);
-
-        const plane = new THREE.Mesh(this.planeGeometry, this.planeMaterial);
+        const plane = new THREE.Mesh(Banner.planeGeometry, this.planeMaterial);
         plane.position.set(2.5, 0, 0);
 
-        // Add text to the banner
-        const loader  = new FontLoader();
-        const textValue = `${this.group.operator}${this.group.value}`
-        loader.load('/fonts/optimer_bold.typeface.json', (font: Font) => {
-            const textGeometry = new TextGeometry(textValue, {
-                font: font,
-                size: 1.8,
-                depth: 0.1,
-            })
-            textGeometry.computeBoundingBox();
+        if (!Banner.font) {
+            await this.loadFont();
+        }
 
-            const textWidth = textGeometry.boundingBox ? textGeometry.boundingBox.max.x - textGeometry.boundingBox.min.x : 0;
+        if (Banner.font) {
+            const operator = this.getRandomOperator();
+            const value = this.getRandomValue();
+            const textValue = `${operator}${value}`;
 
-            const textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000})
-            const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-            textMesh.position.set(-textWidth/2, -0.5, 0.1);
-            plane.add(textMesh);
-        })
+            this.group.userData = {operator, value}
+            this.addTextToPlane(plane, textValue, Banner.font);
+        }
 
-        this.group.add(pole1, pole2, plane)
-
-        this.gameScene.add(this.group);
+        this.group.add(pole1, pole2, plane);
+        Banner.banners.push(this.group);
     }
 
-    getRandomOperator = () => {
-        const operators = ['x', '+', '-', '÷'];
-        return operators[Math.floor(Math.random() * operators.length)]
+    async addTextToPlane(plane: THREE.Mesh, textValue: string, font: Font) {
+        const textGeometry = new TextGeometry(textValue, { font: font, size: 1.8, depth: 0.1 });
+        const textMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+
+        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+        textMesh.position.set(-1.5, -0.5, 0.1);
+        plane.add(textMesh);
     }
 
+    async loadFont() {
+        const loader = new FontLoader();
+        try {
+            Banner.font = await loader.loadAsync('/fonts/optimer_bold.typeface.json');
+        } catch (error) {
+            console.error("Failed to load font:", error);
+        }
+    }
+
+    getRandomOperator = () => ['x', '+', '-', '÷'][Math.floor(Math.random() * 4)];
     getRandomValue = () => Math.ceil(Math.random() * 9);
+    getRandomColor = () => `#${Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0')}`;
 
-    getRandomColor = () => {
-        return '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+    static initBanners(gameScene: THREE.Scene) {
+        const banner1 = new Banner();
+        const banner2 = new Banner();
+
+        const [x1, x2] = this.getNonOverlappingPositions(-10, 5, 7);
+
+        banner1.group.position.set(x1, 2, 0);
+        banner2.group.position.set(x2, 2, 0);
+
+        const bannerGroup = new THREE.Group();
+        bannerGroup.add(banner1.group, banner2.group);
+
+        Banner.banners.push(bannerGroup);
+        bannerGroup.position.z = -150;
+        gameScene.add(bannerGroup);
+    }
+
+    updateBanners(character: Character) {
+        const bannerSpeed = 0.5;
+        if (this.time.elapsed - Banner.lastBannerTime > Banner.bannerInterval) {
+            Banner.initBanners(this.gameScene);
+            Banner.lastBannerTime = this.time.elapsed;
+        }
+
+        Banner.banners.forEach((bannerGroup, index) => {
+            bannerGroup.position.z += bannerSpeed;
+
+            for (let j = 0; j < bannerGroup.children.length; j++) {
+                const banner = bannerGroup.children[j] ;
+
+                if (character.model && banner.position.z >= character.model.position.z && character.checkCollision(banner)) {
+                    this.handleCollision(banner);
+                    bannerGroup.remove(banner);
+                }
+            }
+
+            if (character.model && bannerGroup.position.z > character.model.position.z + 5) {
+                this.gameScene.remove(bannerGroup);
+                Banner.banners.splice(index, 1);
+            }
+        });
+    }
+
+    handleCollision(banner: THREE.Object3D<THREE.Object3DEventMap>) {
+        const { operator, value } = banner.userData;
+        this.world.calculateScore(operator, value)
+        this.gameScene.remove(banner);
     }
 
     static getNonOverlappingPositions(minX: number, maxX:number, minDistance:number) {
@@ -99,8 +152,16 @@ export default class Banner {
         return [x1, x2];
     }
 
+
     private get gameScene() {
         return this.experience.gameScene;
     }
 
+    private get time() {
+        return this.experience.time;
+    }
+
+    private get world() {
+        return this.experience.world;
+    }
 }
--- src/Experience/World/World.ts
+++ src/Experience/World/World.ts
@@ -1,57 +1,34 @@
 import Experience from "../Experience";
 import Character from "./Character.ts";
-import Enemy from './Enemy'
+import Enemy from './Enemy';
 import Environment from "./Environment";
-import RoadSet from './RoadSet'
-import Banner from './Banner'
-import * as THREE from "three";
-
-class CustomBanner extends THREE.Mesh {
-  operator: string;
-  value: number;
-
-  constructor(geometry: THREE.BufferGeometry, material: THREE.Material, value: number, operator: string) {
-    super(geometry, material);
-    this.value = value;
-    this.operator = operator;
-  }
-}
-
-// Represents a group of banners
-class CustomBannerGroup extends THREE.Group {}
-
-// class CustomEnemyGroup extends THREE.Group {}
+import RoadSet from './RoadSet';
+import Banner from './Banner';
 
 export default class World {
   experience: Experience;
-  banners: CustomBannerGroup[];
-  enemies: CustomBannerGroup[];
+  bannerManager?: Banner;
   roadSets: RoadSet[];
   character?: Character;
   enemy?: Enemy;
   environment?: Environment;
-  lastBannerTime: number;
-  bannerInterval: number;
-  lastEnemyTime: number;
   score: number;
-
+  bannerInitialized: boolean;
+  bannerStartTime: number;
 
   constructor() {
     this.experience = Experience.getInstance();
     this.roadSets = [];
-    this.banners = [];
-    this.enemies = [];
     this.score = 10;
-    this.lastBannerTime = 0;
-    this.bannerInterval = 2500; // 2.5 seconds
-    this.lastEnemyTime = 0;
+    this.bannerInitialized = false;  // Tracks if banners are already initialized
+    this.bannerStartTime = 0;        // Tracks the time when to start banner creation
 
     this.resources.on("ready", () => {
-      // Setup
       this.initRoadSets();
       this.initEnemies();
       this.character = new Character();
       this.environment = new Environment();
+      this.bannerStartTime = this.time.elapsed;  // Set the banner start time
     });
   }
 
@@ -60,97 +37,24 @@ export default class World {
   }
 
   update() {
-    const initialInterval = 2500
-    const minInterval = 1000;
-    const timeFactor = 0.1;
-
-    if (this.roadSets) {
-      const baseSpeed = 0.15;
-      // 바닥 움직이기
-      this.roadSets.forEach((roadSet: RoadSet) => roadSet.update(baseSpeed))
-    }
+    const baseSpeed = 0.15;
+    this.roadSets.forEach((roadSet) => roadSet.update(baseSpeed));
 
     if (this.character) {
       this.character.update();
-    }
 
-    // if (this.enemies) {
-    //   this.enemies.forEach((enemyGroup) => {
-    //     enemyGroup.children.forEach((enemy) => {
-    //       console.log("ENEMY", enemy)
-    //     })
-    //   })
-    // }
-
-    // if (this.enemies) {
-    //   // Check if it's time to create new banners
-    //   if (this.time.elapsed - this.lastEnemyTime > this.bannerInterval) {
-    //     this.initEnemies();
-    //     this.lastEnemyTime = this.time.elapsed;
-    //   }
-    //
-    //   // Move Enemies
-    //   const enemySpeed = 0.5;
-    //   for (let i = this.enemies.length - 1; i >= 0; i--) {
-    //     const enemyGroup = this.enemies[i];
-    //     enemyGroup.position.z += enemySpeed;
-    //
-    //     if (!(this.character && this.character.model)) return;
-    //
-    //     // Check collision of each banner with the character
-    //     for (let i = 0; i < enemyGroup.children.length; i++) {
-    //       const enemy = enemyGroup.children[i] as CustomBanner;
-    //       if (enemyGroup.position.z === this.character.model.position.z && this.character.checkCollision(enemy)) {
-    //
-    //         // const bannerValue = enemy.value;
-    //         enemyGroup.remove(enemy);
-    //       }
-    //     }
-    //
-    //     // Remove banner if it's passed the character's position + 5
-    //     if (enemyGroup.position.z > this.character.model.position.z + 5) {
-    //       this.gameScene.remove(enemyGroup);
-    //       this.enemies.splice(i, 1);
-    //     }
-    //   };
-    // }
-
-    if (this.banners && this.character && this.character.model) {
-      // this.bannerInterval =  Math.max(initialInterval - this.time.elapsed * timeFactor, minInterval);
-      this.bannerInterval =  Math.max(initialInterval - this.time.elapsed * timeFactor, minInterval);
-
-      // Check if it's time to create new banners
-      if (this.time.elapsed - this.lastBannerTime > this.bannerInterval) {
-        this.initBanners();
-        this.lastBannerTime = this.time.elapsed;
+      // Trigger banner initialization after 2.5 seconds
+      if (!this.bannerInitialized && (this.time.elapsed - this.bannerStartTime) > 2500) {
+        this.bannerManager = new Banner();
+        this.bannerInitialized = true;
+        return;
       }
 
-      // Move banners
-      const bannerSpeed = 0.5;
-      for (let i = this.banners.length - 1; i >= 0; i--) {
-        const bannerGroup = this.banners[i];
-        bannerGroup.position.z += bannerSpeed;
-
-        // Check collision of each banner with the character
-        for (let i = 0; i < bannerGroup.children.length; i++) {
-          const banner = bannerGroup.children[i] as CustomBanner;
-          if (bannerGroup.position.z === this.character.model.position.z && this.character.checkCollision(banner)) {
-
-            const bannerValue = banner.value;
-            const bannerOperator = banner.operator;
-            this.calculateScore(bannerOperator, bannerValue)
-            bannerGroup.remove(banner);
-          }
-        }
-
-        // Remove banner if it's passed the character's position + 5
-        if (bannerGroup.position.z > this.character.model.position.z + 5) {
-          this.gameScene.remove(bannerGroup);
-          this.banners.splice(i, 1);
-        }
-      };
+      // Update banners if they exist
+      if (this.bannerManager) {
+        this.bannerManager.updateBanners(this.character);
+      }
     }
-
   }
 
   initRoadSets() {
@@ -162,54 +66,12 @@ export default class World {
       const roadSet = new RoadSet();
       roadSet.group.position.z = initialPositionZ + i * spacing;
       this.roadSets.push(roadSet);
-      this.gameScene.add(roadSet.group)
+      this.gameScene.add(roadSet.group);
     }
   }
 
-  initBanners() {
-    const minX = -10;
-    const maxX = 5;
-    const bannerWidth = 5;
-    const minDistance = bannerWidth + 1; // buffer in between the banners
-
-    const [x1, x2] = Banner.getNonOverlappingPositions(minX, maxX, minDistance);
-
-    const banner1 = new Banner();
-    banner1.group.position.set(x1, 2, 0)
-
-    const banner2 = new Banner();
-    banner2.group.position.set(x2, 2, 0)
-
-    const bannerGroup = new CustomBannerGroup();
-    bannerGroup.position.z = -100;
-    bannerGroup.add(banner1.group, banner2.group);
-    this.banners.push(bannerGroup)
-    this.gameScene.add(bannerGroup);
-  }
-
-  initEnemies () {
-    const minX = -10;
-    const maxX = 5;
-    const enemyWidth = 5;
-    const minDistance = enemyWidth + 1; // buffer in between the enemies
-
-    const [x1, x2] = Enemy.getNonOverlappingPositions(minX, maxX, minDistance);
-
-    const enemy1 = new Enemy();
-    const enemy2 = new Enemy();
-
-    enemy1.group.position.set(x1, 2, 0)
-    enemy2.group.position.set(x2, 2, 0)
-
-    const enemyGroup = new CustomBannerGroup();
-    enemyGroup.position.z = -100
-
-    // console.log("POS", enemy1.group.position)
-
-    enemyGroup.add(enemy1.group, enemy2.group);
-    console.log('Enemy group children:', enemyGroup.children);
-    this.enemies.push(enemyGroup);
-    this.gameScene.add(enemyGroup);
+  initEnemies() {
+    // Similar logic for initializing enemies
   }
 
   updateScoreDisplay() {
@@ -219,51 +81,31 @@ export default class World {
     }
   }
 
-  async calculateScore(operator: string, value: number) {
+  calculateScore(operator: string, value: number) {
     switch (operator) {
-      case '+':
-        this.score += value;
-        break;
-      case '-':
-        this.score -= value;
-        break;
-      case 'x':
-        this.score *= value;
-        break;
-      case '÷':
-        this.score /= value;
-        break;
+      case '+': this.score += value; break;
+      case '-': this.score -= value; break;
+      case 'x': this.score *= value; break;
+      case '÷': this.score /= value; break;
     }
-
-    // Update the score
     this.updateScoreDisplay();
 
-    if (this.score <= 0) {
-      setTimeout(async () => {
-        if (this.character) {
-          await this.character.play("death");
-        }
-        setTimeout(() => {
-          window.alert("Game Over!\nThe game will restart once the alert is closed.");
-          this.resetGame();
-        }, 700)
-      })
+    if (this.score <= 0 && this.character) {
+      this.character.play('death');
+
+      setTimeout(() => {
+        window.alert("Game Over!\nThe game will restart once the alert is closed.");
+        this.resetGame();
+      }, 700);
     }
   }
 
   resetGame() {
-    // Reset start time
     this.time.elapsed = 0;
 
     // Reset score
     this.score = 10;
     this.updateScoreDisplay();
-
-    // Remove all existing banners
-    this.banners.forEach((bannerGroup) => {
-      this.gameScene.remove(bannerGroup);
-    });
-    this.banners = [];
 
     // Remove all road sets
     this.roadSets.forEach((roadSet) => {
@@ -275,13 +117,9 @@ export default class World {
     this.initRoadSets();
 
     if (this.character) {
+      this.character.play('walking')
       this.character.resetPosition();
-      this.character.play("walking");
     }
-
-    // Reset the last banner creation time
-    this.lastBannerTime = 0;
-    this.bannerInterval = 2500;
   }
 
   private get gameScene() {
@@ -291,5 +129,4 @@ export default class World {
   private get time() {
     return this.experience.time;
   }
-
 }
