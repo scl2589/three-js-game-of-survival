6b45cc91a6c5bc73f083b1642658d01ad5e581b3
Chae Lin Shin <chaelinshin96@gmail.com>
2024-10-22 10:04:59 +0900

    Update camera angle

--- src/Experience/Camera.ts
+++ src/Experience/Camera.ts
@@ -33,8 +33,10 @@ export default class Camera {
     this.controls.enableDamping = true;
     this.controls.dampingFactor = 0.25;
     this.controls.enableZoom = true;
-    this.controls.maxPolarAngle = Math.PI / 2.1;
-    this.controls.minPolarAngle = Math.PI / 2.25;
+    this.controls.maxPolarAngle = Math.PI / 2.1; // up limit
+    this.controls.minPolarAngle = Math.PI / 2.3; // down limit
+    this.controls.minAzimuthAngle = -Math.PI / 10;  // Left limit
+    this.controls.maxAzimuthAngle = Math.PI / 10;   // Right limit
     this.controls.minDistance = 5;
     this.controls.maxDistance = 50;
   }
